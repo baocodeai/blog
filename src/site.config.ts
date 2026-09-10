1a59ca26ffceaea12ef85c85751c1f45a593a8d3
nvbao117 <baocute056@gmail.com>
2026-09-10 08:42:51 +0700

    don

---
+++ src/site.config.ts
@@ -0,0 +1,11 @@
+/**
+ * Cấu hình chung cho toàn bộ website BaoNV
+ */
+export const SITE_CONFIG = {
+  /**
+   * Bật/tắt hiển thị toàn bộ mục Projects trên website (Menu điều hướng, Section trang chủ, Footer).
+   * - false: Ẩn mục Projects
+   * - true:  Hiện lại mục Projects
+   */
+  showProjects: false,
+};
